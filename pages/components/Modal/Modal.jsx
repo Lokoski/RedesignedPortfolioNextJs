5dc92d9bc687d415cefc50a1f852798b06725dfc
Alex <aleksandar.lokoski@gmail.com>
2021-11-06 15:38:10 -0400

    completed w/o navbar

--- pages/components/Modal/Modal.jsx
+++ pages/components/Modal/Modal.jsx
@@ -6,27 +6,29 @@ const Modal = ({ data, projectId, setOpenModal }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.modalCard}>
-        <div className={styles.exitButton}>
-          <button onClick={() => setOpenModal(false)}> x </button>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image src={filteredData.image} alt="" />
-        </div>
-        <div className={styles.contentContainer}>
-          <div className={styles.title}>
-            <h1>{filteredData.title}</h1>
+      {filteredData && (
+        <div className={styles.modalCard}>
+          <div className={styles.exitButton}>
+            <button onClick={() => setOpenModal(false)}> x </button>
           </div>
-          <div className={styles.description}>
-            <p>{filteredData.description}</p>
+          <div className={styles.imageContainer}>
+            <Image src={filteredData.image} alt="" />
           </div>
-          <div className={styles.buttons}>
-            <button>Github</button>
+          <div className={styles.contentContainer}>
+            <div className={styles.title}>
+              <h1>{filteredData.title}</h1>
+            </div>
+            <div className={styles.description}>
+              <p>{filteredData.description}</p>
+            </div>
+            <div className={styles.buttons}>
+              <button>Github</button>
 
-            {filteredData.liveDemo && <button>Live Demo</button>}
+              {filteredData.liveDemo && <button>Live Demo</button>}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
